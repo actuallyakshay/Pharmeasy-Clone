e81faa8890aa71092cbe3ccc04355ae15961d70d
actuallyakshay <akshay.rajput1197@gmail.com>
2022-12-22 23:26:55 +0530

    added frontend with typescript

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,10 +1,1 @@
-import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import thunk from "redux-thunk";
-import { productReducer } from "./Product/product.reducer";
 
-const rootReducer = combineReducers({
-  product: productReducer,
-});
-
-const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
-export default store;
--- client/src/organisam/Footer.jsx
+++
@@ -1,7 +0,0 @@
-import { Box } from "@chakra-ui/react";
-
-const Footer = () => {
-  return <Box></Box>;
-};
-
-export default Footer;
