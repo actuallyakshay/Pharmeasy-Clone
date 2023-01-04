1ed0aef6d60658f78227d7981e488a3f64c5cc8d
actuallyakshay <akshay.rajput1197@gmail.com>
2023-01-04 11:42:49 +0530

    login functionality has been completed

--- client/src/Redux/Auth/authReducer.ts
+++ client/src/Redux/Auth/authReducer.ts
@@ -2,8 +2,13 @@ import { IAuth } from "../../@types/IAuth";
 import { IAuthAction } from "./auth.actions";
 import { IAuthtypes } from "./auth.type";
 
+let token = localStorage.getItem("token") || "";
+
 const iState: IAuth = {
   adminAuth: false,
+  userLoading: false,
+  userOTP: "",
+  token,
   userAuth: false,
 };
 
@@ -23,6 +28,28 @@ export const authReducer = (state = iState, action: IAuthAction) => {
         adminAuth: false,
       };
 
+    case IAuthtypes.GET_USER_LOGIN_LOADING:
+      return {
+        ...state,
+        userLoading: true,
+      };
+
+    case IAuthtypes.GET_USER_LOGIN_SUCCESS:
+      localStorage.setItem("token", action.payload?.token);
+      return {
+        ...state,
+        userLoading: false,
+        userOTP: String(action.payload.otp),
+        token: action.payload?.token,
+      };
+
+    case IAuthtypes.GET_USER_LOGOUT:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        userOTP: "",
+        token: "",
+      };
     default:
       return state;
   }
