ba6d97eb28ae53db3045733144fd5753bae2728a
Manoj Rathod <myraysever@gmail.com>
2023-01-06 20:51:06 +0530

    updated code

--- client/src/Components/LabTest/AllTest.tsx
+++ client/src/Components/LabTest/AllTest.tsx
@@ -14,8 +14,6 @@ const AllTest = () => {
       .then((res) => setTest(res.data))
       .catch((error) => console.log("Error", error));
   };
-
-  // console.log(test);
   useEffect(() => {
     getTest();
   }, []);
